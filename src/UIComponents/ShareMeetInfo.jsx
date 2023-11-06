9558dca4dbcfeac6abaeb02b8365b77326f6ff64
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-11-07 01:44:13 +0530

    Beta Done

--- src/UIComponents/ShareMeetInfo.jsx
+++ src/UIComponents/ShareMeetInfo.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ShareIcon from '@mui/icons-material/Share';
 
-const ShareMeetingInfo = ({ peerId }) => {
+const ShareMeetingInfo = ({peerId}) => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   const shareMeeting = () => {
     if (navigator.share) {
-      const message = `Hey there! Let's catch up on For You. You can watch YouTube together and have a face-to-face chat. Connect with me using this meeting link. My peer ID is ${peerId}.`;
-
-      navigator.clipboard.writeText(peerId);
-      
+      const shareableLink = window.location.href;
       navigator.share({
         title: 'Join my meeting on For You',
-        text: message,
-        
+        text: `Hey there! Let's catch up on For You. You can watch YouTube together and have a face-to-face chat. Connect with me using this meeting link. My meeting is ${peerId}`,
+        url: shareableLink
       })
         .then(() => console.log('Successfully shared meeting info.'))
         .catch((error) => console.error('Error sharing meeting info:', error));
